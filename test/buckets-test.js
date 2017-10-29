a64a0ac9ddadbcd92635bfb4d143627b4345d853
coleworsley <cole.s.worsley@gmail.com>
2017-10-29 12:50:58 -0600

    add test for fill bucket

--- test/buckets-test.js
+++ test/buckets-test.js
@@ -3,7 +3,7 @@ const make_soup = require('../problems/buckets').make_soup;
 const Bucket = require('../problems/buckets').Bucket;
 // import {make_soup, Bucket} from '../problems/buckets';
 
-describe('Make Soup', () => {
+describe('Buckets', () => {
   it('should have a class for a bucket', () => {
     const bucket = new Bucket(3);
 
@@ -15,5 +15,16 @@ describe('Make Soup', () => {
 
     expect(bucket).to.have.a.property('size');
     expect(bucket).to.have.a.property('quantity');
-  })
+  });
+
+  it('should have a function to fill the bucket', () => {
+    const bucket = new Bucket(3);
+
+    expect(bucket.size).to.equal(3);
+    expect(bucket.quantity).to.equal(0);
+
+    bucket.fill();
+
+    expect(bucket.quantity).to.equal(3);
+  });
 });
