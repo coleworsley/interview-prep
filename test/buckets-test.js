0d9441b45e3aec6ee3426b5812f495aa411ab4e8
coleworsley <cole.s.worsley@gmail.com>
2017-10-30 13:24:39 -0600

    add tests for transferring fluid

--- test/buckets-test.js
+++ test/buckets-test.js
@@ -42,4 +42,37 @@ describe('Buckets', () => {
 
     expect(bucket.quantity).to.equal(0);
   });
+
+  it('should be able to transfer from one bucket to another', () => {
+    const bucket_one = new Bucket(3);
+    const bucket_two = new Bucket(5);
+
+    expect(bucket_one.size).to.equal(3);
+    expect(bucket_two.size).to.equal(5);
+    expect(bucket_one.quantity).to.equal(0);
+    expect(bucket_two.quantity).to.equal(0);
+
+    bucket_one.fill();
+
+    expect(bucket_one.quantity).to.equal(3);
+
+    bucket_one.transfer(bucket_two);
+
+    expect(bucket_one.quantity).to.equal(0);
+    expect(bucket_one.quantity).to.equal(3);
+  });
+
+  it('should only fill up to the brim of each bucket', () => {
+    const bucket_one = new Bucket(3);
+    const bucket_two = new Bucket(5);
+
+    bucket_two.fill();
+
+    expect(bucket_two.quantity).to.equal(5);
+
+    bucket_two.transfer(bucket_one);
+
+    expect(bucket_one.quantity).to.equal(3);
+    expect(bucket_one.quantity).to.equal(2);
+  });
 });
